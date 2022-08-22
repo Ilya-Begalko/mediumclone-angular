131f8bc81b6ad7566a2f04aa31489c2b8c2e1172
Ilya Begalko <ilja.begalko@yandex.ru>
2022-08-22 21:59:37 +0600

    Дебаж NgRx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import {NgModule} from '@angular/core'
 import {BrowserModule} from '@angular/platform-browser'
 
+import {StoreModule} from '@ngrx/store'
+import {StoreDevtoolsModule} from '@ngrx/store-devtools'
+import {environment} from 'src/environments/environment'
+
 import {AppRoutingModule} from './app-routing.module'
 import {AppComponent} from './app.component'
-import {AuthModule} from './auth/auth.module';
-import { StoreModule } from '@ngrx/store'
+import {AuthModule} from './auth/auth.module'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, AuthModule, StoreModule.forRoot({}, {})],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AuthModule,
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
