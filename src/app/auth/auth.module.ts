094fa86e9b9d5045811f285ddecf69244a7fcff2
Ilya Begalko <ilja.begalko@yandex.ru>
2022-08-22 20:52:17 +0600

    Модуль регистрации

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,17 @@
+import {CommonModule} from '@angular/common'
+import {NgModule} from '@angular/core'
+import {RouterModule, Routes} from '@angular/router'
+import {RegisterComponent} from './components/register/register.component'
+
+const routes: Routes = [
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+]
+
+@NgModule({
+  imports: [CommonModule, RouterModule.forChild(routes)],
+  declarations: [RegisterComponent],
+})
+export class AuthModule {}
