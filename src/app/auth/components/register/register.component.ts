2fb8187809cd649d177adada475fb08a8094023a
Ilya Begalko <ilja.begalko@yandex.ru>
2022-08-22 21:41:57 +0600

    Добавление NgRX

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,6 +1,10 @@
 import {Component, OnInit} from '@angular/core'
 import {FormBuilder, FormGroup, Validators} from '@angular/forms'
 
+import {Store} from '@ngrx/store'
+
+import {registerAction} from '../../store/actions/register.action'
+
 @Component({
   selector: 'mc-register',
   templateUrl: './register.component.html',
@@ -9,7 +13,7 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms'
 export class RegisterComponent implements OnInit {
   form!: FormGroup
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private store: Store) {}
 
   ngOnInit(): void {
     this.initializeForm()
@@ -19,11 +23,12 @@ export class RegisterComponent implements OnInit {
     this.form = this.fb.group({
       username: ['', Validators.required],
       email: '',
-      password: ''
+      password: '',
     })
   }
 
-  onSubmit():void{
+  onSubmit(): void {
     console.log(this.form.value)
+    this.store.dispatch(registerAction(this.form.value))
   }
 }
