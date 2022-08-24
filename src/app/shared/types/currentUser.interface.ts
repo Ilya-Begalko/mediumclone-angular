978ab62b3bad7021abe30e4143ab9ae5e8dde428
Ilya Begalko <ilja.begalko@yandex.ru>
2022-08-24 19:39:00 +0600

    Создание интерфейсов для модуля авторизации

---
+++ src/app/shared/types/currentUser.interface.ts
@@ -0,0 +1,10 @@
+export interface CurrentUserInterface {
+  id: number
+  email: string
+  cretedAt: string
+  updatedAt: string
+  username: string
+  bio: string | null
+  image: string | null
+  token: string
+}
